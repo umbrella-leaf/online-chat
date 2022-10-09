55ad662dc517d6aa1143b30d1ce1fda8fbdc65b4
Liu Zhongnuo <2313678365@qq.com>
2022-10-09 23:15:45 +0800

    修改顶栏用户头像样式

--- online-chat-backend/user/view.py
+++ online-chat-backend/user/view.py
@@ -2,9 +2,42 @@ from flask import Blueprint, request, g, jsonify
 from utils.dbOperation import MySQL
 from utils.Response import Error, Success, Warn
 from utils.Token import Token
-from exts import auth_user, sms
+from exts import auth_user, auth_token
 
 user_route = Blueprint('user', __name__)
+
+
+@user_route.route('/get-info', methods=['GET'])
+@auth_token.login_required
+def getUserInfo():
+    telephone = g.telephone
+    # 查询是否有手机号
+    res = MySQL.checkTel(telephone)
+    if res.status != 200:
+        return jsonify(Error.error.to_dict())
+    # 如果没有，则报错
+    if not res.data:
+        return jsonify(Error(message="找不到该用户！").to_dict())
+    res = MySQL.getUserInfo(telephone)
+    if res.status != 200:
+        return jsonify(Error.error.to_dict())
+    return jsonify(Success(data=res.data, message="获取用户信息成功！").to_dict())
+
+
+@user_route.route('/get-avatar', methods=['GET'])
+@auth_token.login_required
+def getUserAvatar():
+    telephone = g.telephone
+    res = MySQL.checkTel(telephone)
+    if res.status != 200:
+        return jsonify(Error.error.to_dict())
+    # 如果没有，则报错
+    if not res.data:
+        return jsonify(Error(message="找不到该用户！").to_dict())
+    res = MySQL.getAvatarUrl(telephone)
+    if res.status != 200:
+        return jsonify(Error.error.to_dict())
+    return jsonify(Success(data=res.data, message="获取用户信息成功！").to_dict())
 
 
 @user_route.route('/change-status', methods=['GET'])
