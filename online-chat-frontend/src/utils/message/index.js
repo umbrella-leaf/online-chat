451a0e94ac9f656edf8acc4acd46a2890a818898
Liu Zhongnuo <2313678365@qq.com>
2022-10-09 18:28:57 +0800

    封装了前端提示信息显示函数

---
+++ online-chat-frontend/src/utils/message/index.js
@@ -0,0 +1,17 @@
+import {message} from "ant-design-vue";
+
+export function ResponseToMessage(response, need_suc=true) {
+  if (response.data.status === 200) {
+    if (need_suc) {
+      message.success(response.data.message);
+    }
+  } else if (response.data.status === 300) {
+    message.warning(response.data.message);
+  } else {
+    message.error(response.data.message);
+  }
+}
+
+export function ReportErrorMessage() {
+  message.error('请求发送出错！');
+}
