8650cfa104721856905cf839db07fe90c86efe31
Liu Zhongnuo <2313678365@qq.com>
2022-10-14 21:39:07 +0800

    更新用户搜索路由

--- online-chat-frontend/src/router/SidebarRoutes.js
+++ online-chat-frontend/src/router/SidebarRoutes.js
@@ -1,12 +1,17 @@
 export const SidebarRoutes = [
   {
-    path: '/chatroom',
+    path: '/chat-room',
     name: '聊天室',
     component: () => import("../views/ChatRoom")
   },
   {
-    path: '/profile',
-    name: '用户',
+    path: '/user-profile',
+    name: '个人中心',
     component: () => import("../views/UserProfile")
+  },
+  {
+    path: '/user-add',
+    name: '添加好友',
+    component: () => import("../views/UserSearch")
   }
 ]
