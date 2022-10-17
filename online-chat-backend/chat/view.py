8a443fcb0ed6475133a6ebc9fae7fc70d63ff2e9
Liu Zhongnuo <2313678365@qq.com>
2022-10-17 15:10:36 +0800

    在后端chat路由蓝图中增加消息发送的路由函数

--- online-chat-backend/chat/view.py
+++ online-chat-backend/chat/view.py
@@ -24,10 +24,9 @@ def GetChatList():
     return jsonify(Success(data=chat_list, message="获取聊天列表成功！").to_dict())
 
 
-@chat_route.route('/get-message-list', methods=['POST'])
+@chat_route.route('/get-message-list/<chat_id>', methods=['GET'])
 @auth_token.login_required
-def GetMessageList():
-    chat_id = request.json.get('chat_id')
+def GetMessageList(chat_id):
     if chat_id is None:
         return jsonify(Error(message="聊天ID不能为空!").to_dict())
     res = MySQL.checkChatByID(chat_id)
@@ -41,3 +40,19 @@ def GetMessageList():
     message_list = res.data
     return jsonify(Success(data=message_list, message="聊天消息获取成功！").to_dict())
 
+
+@chat_route.route('/send-new-message/<chat_id>', methods=['POST'])
+@auth_token.login_required
+def SendNewMessage(chat_id):
+    if chat_id is None:
+        return jsonify(Error(message="聊天ID不能为空!").to_dict())
+    content = request.json.get('content')
+    if content is None:
+        return jsonify(Error(message="消息内容不能为空!").to_dict())
+    sender_id = request.json.get('sender_id')
+    if sender_id is None:
+        return jsonify(Error(message="发送者ID不能为空!").to_dict())
+    res = MySQL.sendNewMessage(chat_id, content, sender_id)
+    if res.status != 200:
+        return jsonify(Error.error.to_dict())
+    return jsonify(Success(message="消息发送成功！").to_dict())
