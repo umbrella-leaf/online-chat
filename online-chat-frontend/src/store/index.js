852040a4227490eb70ffd0e977b8a8c7006a80af
Liu Zhongnuo <2313678365@qq.com>
2022-10-17 15:09:47 +0800

    在vuex中删除了chat模块

--- online-chat-frontend/src/store/index.js
+++ online-chat-frontend/src/store/index.js
@@ -3,7 +3,6 @@ import {settings} from "@/store/settings";
 import {urls} from "@/store/urls";
 import {entrance} from "@/store/entrance";
 import {user} from "@/store/user";
-import {chat} from "@/store/chat";
 import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
@@ -19,14 +18,13 @@ export default createStore({
     settings,
     urls,
     entrance,
-    user,
-    chat
+    user
   },
   plugins: [
     createPersistedState({
       // storage: window.sessionStorage,
       key: 'online-chat-client',
-      path: ['settings', 'urls', 'entrance', 'user', 'chat']
+      path: ['settings', 'urls', 'entrance', 'user']
     })
   ]
 })
