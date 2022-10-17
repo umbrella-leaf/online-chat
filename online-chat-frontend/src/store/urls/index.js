61150caf504b6daffc3dd6834f0e285847819298
Liu Zhongnuo <2313678365@qq.com>
2022-10-17 15:09:26 +0800

    在vuex的urls模块中增加了后端websocket地址

--- online-chat-frontend/src/store/urls/index.js
+++ online-chat-frontend/src/store/urls/index.js
@@ -2,7 +2,8 @@ export const urls = {
   namespaced: true,
   state: () => ({
     icon_font_url: '//at.alicdn.com/t/c/font_3686980_wpy0y4xrox.js',
-    backend_url: 'http://localhost:5000'
+    backend_url: 'http://localhost:5000',
+    websocket_url: 'ws://localhost:5000'
   }),
   getters: {
 
