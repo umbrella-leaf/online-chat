cceb7f008f95a65404c1f5311603c2dbbb951e92
Liu Zhongnuo <2313678365@qq.com>
2022-10-21 10:29:46 +0800

    增加QQ表情解析函数

--- online-chat-frontend/src/utils/emojis/emojiParse.js
+++ online-chat-frontend/src/utils/emojis/emojiParse.js
@@ -1,14 +1,24 @@
-import {defaultEmojisMap1} from "@/utils/emojis/default";
+import {WeChatEmojisMap, QQEmojisMap} from "@/utils/emojis/default";
 
-function getEmojiUrl(emotion) {
+function getWeChatEmojiUrl(emotion) {
   emotion = emotion.split('[')[1].split(']')[0];
-  if (defaultEmojisMap1.hasOwnProperty(emotion)) {
-    let index = defaultEmojisMap1[emotion].match(/\d+/g)[0];
+  if (WeChatEmojisMap.hasOwnProperty(emotion)) {
+    let index = WeChatEmojisMap[emotion].match(/\d+/g)[0];
     return `<img src='https://res.wx.qq.com/mpres/htmledition/images/icon/emotion/${index}.gif' />`;
   }
-  return `[${emotion}]`
+  return `[${emotion}]`;
+}
+
+function getQQEmojiUrl(emotion) {
+  emotion = emotion.split('[')[1].split(']')[0];
+  if (QQEmojisMap.hasOwnProperty(emotion)) {
+    let index = QQEmojisMap[emotion].match(/\d+/g)[0];
+    return `<img src='https://www.emojiall.com/img/platform/qq/${index}@2x.gif' />`;
+  }
+  return `[${emotion}]`;
 }
 
 export function emojiParse(message) {
-  return message.replace(/\[\p{Unified_Ideograph}+\]/ug, getEmojiUrl);
+  return message
+    .replace(/\[\p{Unified_Ideograph}+\]/ug, getWeChatEmojiUrl).replace(/\[\/[a-zA-Z]+]/g, getQQEmojiUrl);
 }
