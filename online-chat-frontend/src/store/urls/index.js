c9778f6d69144855af5958675df3091021ae7ae2
Liu Zhongnuo <2313678365@qq.com>
2022-10-22 09:10:37 +0800

    vuex修改部分url

--- online-chat-frontend/src/store/urls/index.js
+++ online-chat-frontend/src/store/urls/index.js
@@ -1,7 +1,7 @@
 export const urls = {
   namespaced: true,
   state: () => ({
-    icon_font_url: '//at.alicdn.com/t/c/font_3686980_u16jda054a8.js',
+    icon_font_url: '//at.alicdn.com/t/c/font_3686980_aqwr00k5dzk.js',
     backend_url: 'http://localhost:5000',
     websocket_url: 'ws://localhost:5000'
   }),
