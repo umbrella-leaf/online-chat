4143fbd59fb61346727ac14ad8a0feea55c7437c
Liu Zhongnuo <2313678365@qq.com>
2022-10-22 09:11:54 +0800

    增加对用户表情的显示支持，并支持右键菜单保存到本地

--- online-chat-frontend/src/components/Widgets/ChatRoom/MessageFrame.vue
+++ online-chat-frontend/src/components/Widgets/ChatRoom/MessageFrame.vue
@@ -7,9 +7,20 @@
           <div class="main" :class="{self: IsSelfSend(item)}" v-else :key="item.id">
             <a-avatar class="avatar" :src="item.sender_avatar"  alt=""/>
             <span v-if="IsSelfSend(item)" class="unread">{{ IsRead(item) ? "已读" : "未读" }}</span>
-            <div class="content">
+            <div class="content" v-if="item.type === 0">
               <div class="text" v-html="emojiParse(item.content)"></div>
             </div>
+            <a-dropdown :trigger="['contextmenu']" v-else >
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="downloadImage(item.content)">
+                    <DownloadOutlined />
+                    保存到本地
+                  </a-menu-item>
+                </a-menu>
+              </template>
+              <img :src="item.content" alt="" class="emoji"/>
+            </a-dropdown>
           </div>
         </li>
       </ul>
@@ -23,6 +34,8 @@ import Bus from "@/utils/EventBus";
 import {useStore} from "vuex";
 import {msgTimeShowFilter} from "@/utils/time/msgTimeShowFilter";
 import {emojiParse} from "@/utils/emojis/emojiParse";
+import {DownloadOutlined} from "@ant-design/icons-vue";
+import {downloadFile} from "@/utils/emojis/download";
 
 
 const store = useStore();
@@ -50,7 +63,16 @@ const FilterMessageList = computed(() => {
   return msgTimeShowFilter(props.MessageList);
 })
 
+const downloadImage = (fileUrl) => {
+  const imgSrc = fileUrl;
+  const fileName = imgSrc.slice(imgSrc.lastIndexOf('/') + 1); // 下载文件的名字
+  console.log(imgSrc, fileName);
+  downloadFile(imgSrc, fileName);
+}
+
+// 获取消息框组件
 const msg_list = ref();
+// 消息框拉到底
 const msg_list_to_bottom = () => {
   msg_list.value.scrollTop = msg_list.value.scrollHeight;
 }
