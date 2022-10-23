22d64b57fbcdc259aa13a1391b38a74a3705d8e3
Liu Zhongnuo <2313678365@qq.com>
2022-10-23 10:29:01 +0800

    聊天词云生成服务

---
+++ online-chat-backend/utils/WordCloud_service.py
@@ -0,0 +1,51 @@
+import base64
+import jieba
+import wordcloud
+import pandas as pd
+from io import BytesIO
+from PIL import Image
+
+
+def im_2_b64(image):
+    buff = BytesIO()
+    image.save(buff, format="JPEG")
+    img_str = base64.b64encode(buff.getvalue())
+    img_str = str(img_str, "utf-8")
+    return img_str
+
+
+class WordCloud:
+    def __init__(self):
+        self.wordcloud = wordcloud.WordCloud(font_path="msyh.ttc",
+                                             width=1000, height=700,
+                                             background_color='white',
+                                             max_words=300)
+        self.stop_words = ["图片", "表情", "图片表情"]
+
+    def get_word_cloud(self, messages):
+        # panda格式化数据
+        data = pd.DataFrame(messages)
+        data = data[['content']]
+        string = ''
+        for index in data.index:
+            message = data.loc[index]['content']
+            if message != '':
+                string += message
+        # 结巴分词
+        jieba_res = jieba.lcut(string)
+        words_list = []
+        for res in jieba_res:
+            if res not in self.stop_words and len(res) >= 2:
+                words_list.append(res)
+        words_txt = " ".join(words_list)
+        # 生成词云照片
+        self.wordcloud.generate(words_txt)
+        # self.wordcloud.to_file('yun.png')
+        # 转化为base64格式
+        matrix_RGB = self.wordcloud.to_array()
+        image = Image.fromarray(matrix_RGB)
+        return f"data:image/JPEG;base64,{im_2_b64(image)}"
+
+
+
+
