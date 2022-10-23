9bc7308e255da541f0a7116c095475f99a931a62
Liu Zhongnuo <2313678365@qq.com>
2022-10-23 10:27:51 +0800

    QQ表情保存为文本内容时以'<>'包裹'

--- online-chat-frontend/src/utils/emojis/htmlUnParse.js
+++ online-chat-frontend/src/utils/emojis/htmlUnParse.js
@@ -14,14 +14,14 @@ function getQQEmojiName(emoji) {
   const emoji_no = emoji.match(/\d+/g)[0];
   for (const emoji_name in QQEmojisMap) {
     if (QQEmojisMap[emoji_name].includes(emoji_no)) {
-      return `[${emoji_name}]`
+      return `<${emoji_name}>`
     }
   }
   return ``
 }
 
 
-// html格式消息中的表情转化为['表情名']格式
+// html格式消息中的表情转化为['表情名']或<'表情名'>格式
 export function htmlUnParse(html) {
   return html.replace(/<(?!img).*?>/gi, '')
     .replace(/<(?=img).*?wx.*?>/gi, getWeChatEmojiName)
